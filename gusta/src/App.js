fa81a0d8a1a9459c4d9f1c7a228cf3788790c16d
daviddyess <david.dyess@gmail.com>
2019-05-20 01:38:24 -0600

    tests

---
+++ gusta/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <Calculator />
+      </section>
+    </div>
+  );
+}
+
+export default App;
